5006b52201f6894dba2e4ad6bec92d31ac9c43a3
adrianosilva22 <adrianodesilva22@gmail.com>
2024-09-07 02:31:41 -0300

    corrigindo erro de seguranca, armazenando nos cookies o token do usuario ao inves do nameRole

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { isAuthorized } from './accessControl'
+import { jwtDecode } from 'jwt-decode'
+import { TokenUserInfo } from './models/tokenUserInfo'
 
 export function middleware(request: NextRequest) {
+
     const url = new URL(request.url)
     const currentPath = url.pathname
-    const userRole = request.cookies.get('userRole')?.value
+    const userRole = request.cookies.get('tokenUserInfo')?.value
 
-    if (!userRole || !isAuthorized(userRole, currentPath)) {
+    if (!userRole) {
         const url = new URL('/login', request.url)
         return NextResponse.redirect(url)
-    }
+    } else {
+        const tokenDecoded = jwtDecode(userRole) as TokenUserInfo
 
-    return NextResponse.next()
+        if (!tokenDecoded.role || !isAuthorized(tokenDecoded.role, currentPath)) {
+            const url = new URL('/login', request.url)
+            return NextResponse.redirect(url)
+        } else {
+            return NextResponse.next()
+        }
+    }
 }
 
 export const config = {
@@ -23,7 +33,6 @@ export const config = {
         '/empresa/:path*',
         '/mentor/:path*',
         '/dashboard/:path*',
-        '/mentor/:path*',
         '/user/:path*',
     ],
 };
