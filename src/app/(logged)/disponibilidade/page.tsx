fcf710cec56e64346e1a80246b3f0a8672736c37
Gabriel Paiva <gabrielmpaiva0703@gmail.com>
2024-10-31 13:59:02 -0300

    DISPONIBILIDADE

---
+++ src/app/(logged)/disponibilidade/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { useEffect, useRef, useState } from "react";
+import { Calendar } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+
+export default function FullCalendarComponent() {
+  const calendarRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (calendarRef.current) {
+      const calendar = new Calendar(calendarRef.current, {
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: "dayGridMonth",
+        events: [
+          { title: "Event 1", date: "2023-02-28" },
+          { title: "Event 2", date: "2023-03-02" },
+        ],
+      });
+
+      calendar.render();
+    }
+  }, []);
+
+  return <div ref={calendarRef}></div>;
+}
+
