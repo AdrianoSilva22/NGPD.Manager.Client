a7d16120a7340d6c79bbecadde332b03e35161c4
adrianosilva22 <adrianodesilva22@gmail.com>
2024-11-04 20:37:16 -0300

    alterando layout e not-found pra teste de deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,22 @@
-"use client"
-import '@fortawesome/fontawesome-free/css/all.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'feather-icons-react/build/FeatherIcon';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import 'toastr/build/toastr.css';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const metadata: Metadata = {
-  title: "Projeto NGPD",
-  description: "Projeto criado para o NGPD",
+    title: "Projeto NGPD",
+    description: "Projeto criado para o NGPD",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-      <html lang="en">
-        <body className={inter.className}>{children}
-        </body>
-      </html>
-  );
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
 }
