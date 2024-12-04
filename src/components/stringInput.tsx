fc24bdff866a80fde10cbc467e17cbaa1aee1340
Gabriel Paiva <gabrielmpaiva0703@gmail.com>
2024-12-04 13:07:07 -0300

    Ajuste para aceitar numero no input

--- src/components/stringInput.tsx
+++ src/components/stringInput.tsx
@@ -1,29 +1,31 @@
 import { ChangeEvent, useState } from "react";
 
-export const Input = ({ onChange, value,readOnly }: StringInputProps) => {
-    const [error, setError] = useState<string>('');
+export const Input = ({ onChange, value, readOnly }: StringInputProps) => {
+    const [error, setError] = useState<string>("");
 
     const fetchValue = (e: ChangeEvent<HTMLInputElement>) => {
-        const newValue = e.target.value
-        const isString = /^[a-zA-Z ]+$/
-        onChange(newValue)
+        const newValue = e.target.value;
+        // Permite apenas letras e números
+        const isValid = /^[a-zA-Z0-9 ]*$/;
+        onChange(newValue);
 
-        if (!isString.test(newValue)) {
-            setError("Por favor, insira apenas letras do alfabeto.")
+        if (!isValid.test(newValue)) {
+            setError("Por favor, insira apenas letras e números.");
         } else {
-            setError("")
+            setError("");
         }
-    }
+    };
 
     return (
         <div>
-            <input type="text"
+            <input
+                type="text"
                 onChange={fetchValue}
                 value={value}
                 readOnly={readOnly}
-                className={`form-control ${error && 'is-invalid'} `}
+                className={`form-control ${error && "is-invalid"} `}
             />
             {error && <div className="invalid-feedback">{error}</div>}
         </div>
-    )
-}
+    );
+};
